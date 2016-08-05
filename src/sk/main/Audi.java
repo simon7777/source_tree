72bda4fbcd4bad4cc16bea46dfc11ef798b637a3
simon7777 <sdekret@gmail.com>
2016-08-05 11:27:43 +0200

    Update Audi.java

--- src/sk/main/Audi.java
+++ src/sk/main/Audi.java
@@ -3,6 +3,13 @@ package sk.main;
 public class Audi implements Car {
 	private String color;
 	
+	public Audi(String color){
+		this.color = color;
+	}
+	
+	public Audi(){
+	}
+	
 	@Override
 	public boolean color(String color) {
 		this.color = color;
