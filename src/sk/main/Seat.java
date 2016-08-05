d7167a91e4258f1b57af31a83a5c94d94b949c54
simon <sdekret@gmail.com>
2016-08-05 11:21:07 +0200

    Adding seat car

---
+++ src/sk/main/Seat.java
@@ -0,0 +1,17 @@
+package sk.main;
+
+public class Seat implements Car {
+
+	@Override
+	public boolean color(String color) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
