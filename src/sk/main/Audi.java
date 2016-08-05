c167df56676d3cc17a68a9f921d46e3365c24b9e
simon <sdekret@gmail.com>
2016-08-05 11:35:36 +0200

    adding constructor to Audi

--- src/sk/main/Audi.java
+++ src/sk/main/Audi.java
@@ -3,6 +3,13 @@ package sk.main;
 public class Audi implements Car {
 	private String color;
 	
+	public Audi(String color){
+		System.out.println(color);
+	}
+	
+	public Audi(){
+	}
+	
 	@Override
 	public boolean color(String color) {
 		this.color = color;
